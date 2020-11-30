38ef467129c8794f17b07bb2f3c2220584554a45
David Kaufman <david@gigawatt.org>
2020-11-29 19:30:55 -0500

    add checkboxes to menu items

--- src/App.js
+++ src/App.js
@@ -8,24 +8,31 @@ function App() {
         <p class="tagline">
           Caffeinating Commuters since 2020!
         </p>
+
+        <p class="orange">
+          Mobile Order Menu
+        </p>
+
         <p class="orange">
           Coffee <sm>or</sm> Tea
         </p>
         <p class="brown">
+          <input type="checkbox" name="order_item" value="coffee-12oz" />
           12<sm> oz . . . . . . . . . . . .</sm> $1.00
         </p>
         <p class="brown">
+          <input type="checkbox" name="order_item" value="coffee-16oz" />
           16<sm> oz . . . . . . . . . . . .</sm> $1.50
         </p>
         <p class="brown">
-          <div class="multiline">
-            Fill Travel Mug<sm> . . . </sm>$2.00
-          </div>
+          <input type="checkbox" name="order_item" value="coffee-travel_mug" />
+          Fill Travel Mug<sm> . . . </sm>$2.00
         </p>
         <p class="orange">
           Donuts
         </p>
         <p class="brown">
+          <input type="checkbox" name="order_item" value="donut" />
           $1.00
         </p>
       </header>
